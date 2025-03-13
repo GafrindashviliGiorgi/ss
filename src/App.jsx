52b22320b43cd49802702051982159c95a00dc43
Giorgi Gafrindashvili <giorgigafrindashvili190@gmail.com>
2025-03-13 14:07:42 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+const [ricxvi, setRicxvi] = useState(1)
+
+
+  return (
+    <>
+    <div className={ricxvi < 5 ? 'pirveli': 'meore'} id="card">
+      <p style={ricxvi < 5 ? {color:"green"} : {color:"red"}}>{ricxvi < 5  ? "Norm" : "Too Many"}</p>
+    <h1 style={ricxvi < 5 ?{color:'green',  fontFamily:'monospace'}: {color:'red',  fontFamily:'cursive'}}>     {ricxvi}</h1>
+
+    <button onClick={ () => setRicxvi(ricxvi +1)}>+</button>
+
+    <button onClick={ () => setRicxvi(ricxvi -1)}>-</button>
+
+    </div>
+
+    </>
+  )
+}
+
+export default App
+
+
